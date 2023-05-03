494c79635b07ef29ecdd671aeb9fe950dd6d1694
CacoMoore <caroavello.m@gmail.com>
2023-05-03 16:24:48 +0000

    proyect ok

--- src/app.py
+++ src/app.py
@@ -6,7 +6,6 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,6 +13,25 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+jackson_family = FamilyStructure("Jackson")
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": "John" ,
+    "age": 33,
+    "Lucky Numbers": [7, 13, 22]
+}),
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": "Jane" ,
+    "age": 35,
+    "Lucky Numbers": [10, 14, 3]
+}),
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": "Jimmy" ,
+    "age": 5,
+    "Lucky Numbers": [1]
+})
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -25,6 +43,30 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/member', methods=['POST'])
+def handle_new_member():
+
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        raise APIException("Member not found", status_code=404)
+        member = {
+        "id": jackson_family._generateId(),
+        "first_name": request_body.get("first_name", ""),
+        "last_name": "Jackson",
+        "age": request_body.get("age", 0),
+        "lucky_numbers": request_body.get("lucky_numbers", [])
+    }
+    jackson_family.add_member(member)
+
+    response_body = {
+        "id": member["id"],
+        "first_name": member["first_name"],
+        "age": member["age"],
+        "lucky_numbers": member["lucky_numbers"]
+    }
+
+    return jsonify(response_body), 200
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
@@ -34,9 +76,33 @@ def handle_hello():
         "hello": "world",
         "family": members
     }
+    
+    return jsonify(response_body), 200
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def handle_member(member_id):
+
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        raise APIException("Member not found", status_code=404)
+
+    response_body = {
+        "id": member["id"],
+        "first_name": member["first_name"],
+        "age": member["age"],
+        "lucky_numbers": member["lucky_numbers"]
+    }
 
     return jsonify(response_body), 200
+
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def handle_delete_member(member_id):
+    jackson_family.delete_member(member_id)
+    if not deleted:
+        raise APIException("Member not found", status_code=404)
+    return jsonify({"msg": "Member deleted successfully"}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,24 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member["last_name"] = self.last_name
+        member["id"] = self._generateId()
+
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        member["last_name"] = self.last_name
+        member["id"] = self.delete_member()
+
+        self._members.remove(member)
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
